f146dcf46be5f9056f470f86c311961606876fd9
sardistic <coldhunter@gmail.com>
2025-12-08 23:13:38 -0500

    Fix: Bind to 0.0.0.0 and correct /data permissions

--- server/server.js
+++ server/server.js
@@ -214,8 +214,8 @@ app.get('/api/analytics/timeline', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 // --- Last.fm Integration ---
